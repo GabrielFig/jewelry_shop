49cb9eb9c50feb5bcc5ee6bfa81cd141f26f38d8
GabrielFig <jgabriell_00@hotmail.com>
2025-08-08 00:55:32 -0700

    [ADD]implement SqlAlchemyBatchRepository and SqlAlchemyUnitOfWork for database interactions

--- app/adapters/repository.py
+++ app/adapters/repository.py
@@ -1,6 +1,8 @@
 from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Optional
 from app.domain.models import Batch
+from sqlalchemy.orm import Session
+from app.adapters.sqlalchemy_models import BatchModel
 
 
 class AbstractBatchRepository(ABC):
@@ -9,7 +11,7 @@ class AbstractBatchRepository(ABC):
         pass
 
     @abstractmethod
-    def get(self, ref: str) -> Batch:
+    def get(self, ref: str) -> Optional[Batch]:
         pass
 
     @abstractmethod
@@ -29,3 +31,33 @@ class InMemoryBatchRepository(AbstractBatchRepository):
 
     def list(self) -> List[Batch]:
         return list(self._batches)
+
+
+class SqlAlchemyBatchRepository(AbstractBatchRepository):
+    def __init__(self, session: Session):
+        self._session = session
+
+    def add(self, batch: Batch):
+        model = BatchModel(
+            reference=batch.ref,
+            quantity=batch.purchased_quantity,
+            sku=batch.sku
+        )
+        self._session.add(model)
+    def get(self, ref: str) -> Optional[Batch]:
+        model = self._session.query(BatchModel).filter_by(reference=ref).first()
+        print (type(model))
+        if model is None:
+            return None
+        return Batch(
+            ref=str(model.reference),
+            sku=str(model.sku),
+            purchased_quantity=model.quantity  # ← nombre correcto según tu entidad # type: ignore
+        )
+
+
+    def list(self) -> List[Batch]:
+        return [
+            Batch(str(m.reference), str(m.sku),m.quantity) # type: ignore
+            for m in self._session.query(BatchModel).all()
+        ]
--- app/unit_of_work.py
+++ app/unit_of_work.py
@@ -1,6 +1,17 @@
 from abc import ABC, abstractmethod
-from app.adapters.repository import AbstractBatchRepository, InMemoryBatchRepository
+from app.adapters.repository import AbstractBatchRepository, InMemoryBatchRepository, SqlAlchemyBatchRepository
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
 
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./jewelry.db"
+
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False},  # necesario para SQLite
+)
+SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 
 class AbstractUnitOfWork(ABC):
     batches : AbstractBatchRepository
@@ -30,3 +41,25 @@ class InMemoryUnitOfWork(AbstractUnitOfWork):
 
     def rollback(self):
         pass
+
+
+class SqlAlchemyUnitOfWork(AbstractUnitOfWork):
+    def __init__(self):
+        self._session = SessionLocal()
+        self.batches = SqlAlchemyBatchRepository(self._session)
+        self.committed = False
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, *args):
+        if not self.committed:
+            self.rollback()
+        self._session.close()
+
+    def commit(self):
+        self._session.commit()
+        self.committed = True
+
+    def rollback(self):
+        self._session.rollback()
