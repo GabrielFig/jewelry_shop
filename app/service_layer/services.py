d8f374d0d5b85eeb542e3859468670714586d1c9
GabrielFig <jgabriell_00@hotmail.com>
2025-08-14 15:32:18 -0700

    add new events in allocate order function

--- app/service_layer/services.py
+++ app/service_layer/services.py
@@ -1,16 +1,21 @@
 from app.domain.models import OrderLine
 from app.domain.services import allocate
+from app.domain.events import OrderAllocated, OutOfStock
 from app.unit_of_work import AbstractUnitOfWork
+from app.message_bus import publish
 
 
 
-def allocate_order(order_id: str, sku: str, quantity: int, uow: AbstractUnitOfWork) -> str:
-    line = OrderLine(order_id=order_id, sku=sku, quantity=quantity)
+
+def allocate_order(order_id: str, sku: str, qty: int, uow: AbstractUnitOfWork) -> str:
+    line = OrderLine(order_id=order_id, sku=sku, quantity=qty)
     with uow:
         batches = uow.batches.list()
-        batch_ref = allocate(line, batches)
+        batch_ref_tuple = allocate(line, batches)
+        batch_ref = batch_ref_tuple[0]  # Extract the batch reference (str or None)
         if not batch_ref:
-            raise ValueError("Out of stock")
+            publish(OutOfStock(sku=sku))  # 🔴 Evento publicado aquí
+            raise Exception("Out of stock")
+        publish(OrderAllocated(order_id=order_id, sku=sku, quantity=qty, batch_ref=batch_ref))
         uow.commit()
         return batch_ref
-    
