ad5960c2c5f538188f64be62ae57d9a1d34c9018
GabrielFig <jgabriell_00@hotmail.com>
2025-08-14 15:31:28 -0700

    improve in allocate function

--- app/domain/services.py
+++ app/domain/services.py
@@ -1,15 +1,23 @@
 from typing import List, Optional
 from app.domain.models import Batch, OrderLine
+from app.domain import events
 
-def allocate(line: OrderLine, batches: List[Batch]) -> Optional[str]:
-    """
-    Allocates an order line to a batch if possible.
-    
-    :param line: The order line to allocate.
-    :param batches: List of available batches.
-    :return: The reference of the batch if allocation is successful, None otherwise.
-    """
+from typing import Tuple
+
+
+
+
+def allocate(line: OrderLine, batches: List[Batch]):
     for batch in batches:
         if batch.can_allocate(line):
             batch.allocate(line)
-            return batch.ref
+            event = events.OrderAllocated(
+                order_id=line.order_id,
+                sku=line.sku,
+                quantity=line.quantity,
+                batch_ref=batch.ref
+            )
+            return batch.ref, [event]
+    event = events.OutOfStock(sku=line.sku)
+    return None, [event]
+        
